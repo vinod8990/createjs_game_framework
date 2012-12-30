0f855144b8bc59d8afa7ab94d7771119de5d06ec
Vinodkumar KN <iamvinodkumarhtm@gmail.com>
2012-12-30 19:08:46 +0530

    Documentation

---
+++ doc/api.js
@@ -0,0 +1,19 @@
+YUI.add("yuidoc-meta", function(Y) {
+   Y.YUIDoc = { meta: {
+    "classes": [
+        "Assets",
+        "Game",
+        "Scene"
+    ],
+    "modules": [
+        "CGFW"
+    ],
+    "allModules": [
+        {
+            "displayName": "CGFW",
+            "name": "CGFW",
+            "description": "CreateJS Game Framework Module.\nAfter you create a new game instance, you can get the current game instance from anywhere by accessing CGFW.gameInstance property"
+        }
+    ]
+} };
+});
